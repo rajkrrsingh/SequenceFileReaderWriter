b75b04dce65c6005d6ebbd5f12d793ed55d6c7c7
Rajkumar Singh <rajkrrsingh@gmail.com>
2013-10-19 13:29:17 +0530

    updating App.java

--- src/main/java/com/rjkrsinghhadoop/App.java
+++ src/main/java/com/rjkrsinghhadoop/App.java
@@ -3,7 +3,6 @@ package com.rjkrsinghhadoop;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.JobConf;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
